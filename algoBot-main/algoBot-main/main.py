6525f59807fa5338fbaef8642e9e905fb941ec78
matiascasas <56286860+matiascasas@users.noreply.github.com>
2022-07-14 10:17:44 -0300

    Delete algoBot-main/algoBot-main directory

--- algoBot-main/algoBot-main/main.py
+++
@@ -1,8 +0,0 @@
-import bots
-# import pexpect
-
-# child = pexpect.spawn(bots.bot_estrategia1_testing())
-# child.expect('apikey: ')
-# child.expect('secKey: ')
-
-bots.bot_backtesting_estrategia1()
