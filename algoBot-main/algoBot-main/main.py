6447da865477059b46b1707e22bc82521a330d41
matiascasas <56286860+matiascasas@users.noreply.github.com>
2022-07-14 10:15:18 -0300

    Add files via upload

---
+++ algoBot-main/algoBot-main/main.py
@@ -0,0 +1,8 @@
+import bots
+# import pexpect
+
+# child = pexpect.spawn(bots.bot_estrategia1_testing())
+# child.expect('apikey: ')
+# child.expect('secKey: ')
+
+bots.bot_backtesting_estrategia1()
